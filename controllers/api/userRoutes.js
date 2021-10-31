3d1f8aea41774dd5baeacbc07f3454c0cb5e4728
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-31 16:03:54 -0700

    add working user get routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,9 +2,54 @@ const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
 router.get("/", (req, res) => {
-    User.findAll()
-    .then(userData => res.json(userData));
-    
+    User.findAll(
+        {
+            attributes: {
+                exclude: ["password"]
+            }
+        })
+        .then(userData => res.json(userData));
+
+});
+
+router.get("/:id", (req, res) => {
+    User.findOne(
+        {
+            attributes: {
+                exclude: ["password"],
+            },
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: Post,
+                attributes: ["id", "title", "content"]
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text"],
+            }
+            ]
+        })
+        .then(singleUser => {
+            res.json(singleUser);
+        });
+});
+
+router.post("/", (req, res) => {
+    User.create(
+        {
+          username: req.body.username,
+          password: req.body.password  
+        })
+        .then(newUser => {
+            res.session.save(() => {
+                req.session.user_id = newUser.id;
+                req.session.username = newUser.username;
+                req.session.loggedIn = true;
+                res.json(newUser);
+            });
+        });
 });
 
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const helpers = require('./utils/helpers');
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
