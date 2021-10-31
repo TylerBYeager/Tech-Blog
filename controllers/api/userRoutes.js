11073acce8b8a5ea5c32bc4e46988fb51b7c4588
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-31 16:46:40 -0700

    add working post routes user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -36,20 +36,53 @@ router.get("/:id", (req, res) => {
         });
 });
 
-router.post("/", (req, res) => {
-    User.create(
-        {
-          username: req.body.username,
-          password: req.body.password  
-        })
-        .then(newUser => {
-            res.session.save(() => {
-                req.session.user_id = newUser.id;
-                req.session.username = newUser.username;
-                req.session.loggedIn = true;
-                res.json(newUser);
-            });
+router.post("/", async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.loggedIn = true;
+            res.json(newUser);
         });
+    } catch (err) {
+        console.log("error from creating user", err)
+        res.json(err);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password, try again" });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect email or password, try again" });
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true;
+
+            res.json({ user: userData, message: "Successfully logged in" });
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 
