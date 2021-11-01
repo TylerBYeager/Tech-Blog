49a4d613f21a477f08c5924d298c78ab4da7cc6d
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-31 17:02:03 -0700

    add try catch to user routes & add status

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,39 +1,41 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
-router.get("/", (req, res) => {
-    User.findAll(
-        {
-            attributes: {
-                exclude: ["password"]
-            }
-        })
-        .then(userData => res.json(userData));
-
-});
-
-router.get("/:id", (req, res) => {
-    User.findOne(
-        {
-            attributes: {
-                exclude: ["password"],
-            },
-            where: {
-                id: req.params.id
-            },
-            include: [{
-                model: Post,
-                attributes: ["id", "title", "content"]
-            },
+router.get("/", async (req, res) => {
+    try {
+        const userData = await User.findAll(
             {
-                model: Comment,
-                attributes: ["id", "comment_text"],
-            }
-            ]
-        })
-        .then(singleUser => {
-            res.json(singleUser);
-        });
+                attributes: { exclude: ["password"] }
+            });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const singleUser = await User.findOne(
+            {
+                attributes: { exclude: ["password"], },
+                where: { id: req.params.id },
+                include: [{
+                    model: Post,
+                    attributes: ["id", "title", "content"]
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "comment_text"] }
+                ]
+            });
+        if (!singleUser) {
+            res.status(404).json("Sorry but that user doesn't exist. Please input a valid user.");
+        } else {
+            res.status(200).json(singleUser);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.post("/", async (req, res) => {
@@ -62,6 +64,7 @@ router.post("/login", async (req, res) => {
         const validPassword = await userData.checkPassword(req.body.password);
         if (!validPassword) {
             res.status(400).json({ message: "Incorrect email or password, try again" });
+            return;
         }
 
         req.session.save(() => {
