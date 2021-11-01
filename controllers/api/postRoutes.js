8b25558ce4c287afa50f6e8040336a47250c7638
Tyler B Yeager <wow_d2@hotmail.com>
2021-10-31 18:20:06 -0700

    add working routes in posts. Add, update, delete functioning

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
     try {
         const postData = await Post.findAll(
             {
                 attributes: ["id", "title", "content", "created_at"],
-                order: ["created_at"],
                 include: [
                     {
                         model: User,
@@ -50,7 +50,65 @@ router.get("/:id", async (req, res) => {
                     }
                 ]
             });
-        res.status(200).json(singlePost);
+        if (!singlePost) {
+            res.status(404).json("Post ID does not exist. Try again");
+        } else {
+            res.status(200).json(singlePost);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create(
+            {
+                title: req.body.title,
+                content: req.body.content,
+                user_id: req.session.user_id
+            });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+        const updatePost = await Post.update(
+            {
+                title: req.body.tite,
+                content: req.body.content
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            });
+        if (!updatePost) {
+            res.status(404).json("Post ID does not exist. Try again");
+        } else {
+            res.status(200).json(updatePost);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+    try {
+        const deletePost = await Post.destroy(
+            {
+                where: {
+                    id: req.params.id
+                }
+            });
+        if (!deletePost) {
+            res.status(404).json("Post ID does not exist. Try again");
+        } else {
+            res.status(200).json(deletePost);
+        }
     } catch (err) {
         res.status(500).json(err);
     }
