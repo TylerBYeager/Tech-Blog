542d7d5b3bc3b116c24a1a4cfb4a0943cb52ef02
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-01 18:31:27 -0700

    add working get all homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,4 +38,39 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.get("/post/:id", async (req, res) => {
+    try {
+        const onePost = await Post.findOne(
+            {
+                where: { id: req.params.id },
+                attributes: ["id", "content", "title", "created_at"],
+                include: [
+                    {
+                        model: Comment,
+                        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                        include: {
+                            model: User,
+                            attributes: ["username"]
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ["username"]
+                    }
+                ]
+            });
+        if (!onePost) {
+            res.status(404).json("Post does not exist. Try again");
+            return;
+        } else {
+            const post = onePost.get({ plain: true});
+            console.log(post);
+            res.status(200).render("one-post", { post, loggedIn: req.session.loggedIn });
+        }
+    } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+    }
+});
+
 module.exports = router;
